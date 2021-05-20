77ffa74714d7761859a9a29835804a3ae91ff5de
Kamila Silva <kamillasilva140@gmail.com>
2021-05-20 00:16:12 -0300

    exercicio

--- exercicio3.js
+++ exercicio3.js
@@ -1,11 +1,11 @@
 //3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
 
 let jantar= 300
-let valorTotal = jantar + (300/10)
+let valorTotal = jantar + (100/10)
 
 const taxagarçom = valorTotal - jantar
 
-console.log(taxagarçom, valorTotal)
+console.log(`A taxa do garçom: R$ ${taxagarçom} e o valor total a ser pago  ${valorTotal}`)
 
 
 
