a45558e706c3bdd70bcbdbbdf735ded23caab99d
Kamila Silva <kamillasilva140@gmail.com>
2021-05-20 00:17:26 -0300

    exercicio

--- exercicio6.js
+++ exercicio6.js
@@ -1,20 +1,28 @@
 //6 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente. 
 
 let entrada = 1
-if (entrada == 1){
-    console.log(' Olá, hoje é domingo!')
-} if (entrada == 2){
-    console.log(' Olá, hoje é segunda-feira!')
-} if (entrada == 3){
-    console.log('Olá, hoje é terça-feira!')
-} if (entrada == 4){
-    console.log('Olá, hoje é quarta-feira!')
-} if (entrada == 5){
-    console.log('Olá, hoje é quinta-feira!')
-} if (entrada == 6){
-    console.log('Sextou? hoje é sexta-feira')
-} if (entrada == 7){
-    console.log('Olá, hoje é sábado!')
+switch(entrada){
+ case 1:
+   console.log('Olá, hoje é domingo')
+     break
+ case 2:
+   console.log('Olá, hoje é segunda-feira')
+     break
+ case 3:
+    console.log(' Olá, hoje é terça-feira')
+      break
+  case 4:
+    console.log('Olá, hoje é quarta-feira')
+      break
+  case 5:
+    console.log('Olá, hoje é quinta-feira')
+      break
+  case 6:
+    console.log('Olá, hoje é sexta-feira')
+      break
+  case 7:
+    console.log('Olá, hoje é sábado')
+      break
+ default:
+    console.log('Ops.. Insira uma entrada  entre os números 1 e 7')
 }
-
-
