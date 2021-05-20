3877fcd2a31dbf6b9d58d263747492c5afc4649f
Kamila Silva <kamillasilva140@gmail.com>
2021-05-20 00:18:02 -0300

    exercicio

--- exercicio7.js
+++ exercicio7.js
@@ -1,17 +1,14 @@
 //7 - Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais".
 
 let numero1 = 50
-let numero2 = 50
+let numero2 = 60
 
- if (numero1 > numero1){
+ if (numero1 > numero2){
     console.log('O numero é maior')
 } 
- if (numero2 < numero1){
+ else if (numero1 < numero2){
     console.log( 'O numero é menor')
 }
- if (numero1 === 50) {
+ else if (numero1 === numero2){
     console.log('Os numeros são iguais')
 }
-
-
-
